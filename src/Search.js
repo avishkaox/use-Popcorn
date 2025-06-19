c7b3fb1a75226db886a127f79c28fa8526fec171
DESKTOP-7PHAR17\avish <avishka.madushanka123@gmail.com>
2025-06-19 20:44:29 +0530

    finlize the project

--- src/Search.js
+++ src/Search.js
@@ -1,4 +1,19 @@
+import { useEffect, useRef } from "react";
+
 function Search(props) {
+  const inputEl = useRef(null);
+
+  useEffect(function () {
+    function callback(e) {
+      if (e.code === "Enter") {
+        inputEl.current.focus();
+      }
+    }
+
+    document.addEventListener("keydown", callback);
+    return () => document.addEventListener("keydown", callback);
+  }, []);
+
   return (
     <div>
       <input
@@ -7,6 +22,7 @@ function Search(props) {
         placeholder="Search movies..."
         value={props.value}
         onChange={(e) => props.onChange(e.target.value)}
+        ref={inputEl}
       />
     </div>
   );
